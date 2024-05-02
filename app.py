6250ac060e5cf0b451a731d64d1d731b90bed602
JullytheJth <123009257+JullytheJth@users.noreply.github.com>
2024-05-02 18:46:20 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,14 @@ app = Flask(__name__)
 @app.route("/", methods=['GET', 'POST'])
 def index():
     era = []
+    first =[]
+    extinct=[]
+    status=""
+    intro="Select events and geological times from these menus"
     if request.method == 'POST':
         era = request.form.getlist("era")
-    return render_template("index.html", era=era)
+        first = request.form.getlist("first")
+        extinct = request.form.getlist("extinct")
+        status="Submitted"
+        intro=""
+    return render_template("index.html", era=era, first=first, extinct=extinct, status=status, intro=intro)
